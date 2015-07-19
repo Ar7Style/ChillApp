b8551824403c724c41d0b95cb8c418daf0dbddd9
Ar7Style <gstareev@edu.hse.ru>
2015-07-20 00:35:07 +0300

    Settings screen with web sync and analytics

--- CHLSettingsViewController.m
+++ CHLSettingsViewController.m
@@ -18,7 +18,11 @@
 #import "GAITracker.h"
 #import "CHLFriendsListViewController.h"
 
-@interface CHLSettingsViewController ()
+@interface CHLSettingsViewController () {
+    NSMutableArray *json;
+
+}
+
 
 @end
 
@@ -179,85 +183,65 @@ static NSString* ksettingsPassword = @"password";
     [_passwordField.text stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
     
     
-   // NSMutableURLRequest *request = [[NSMutableURLRequest alloc]initWithURL:[NSURL URLWithString: [NSString stringWithFormat:@"http://api.iamchill.co/v1/users/index"]]];
-//    //            [request setValue:@"Chill" forHTTPHeaderField:@"User-Agent"];
-//    [request setValue:[[NSUserDefaults standardUserDefaults] valueForKey:@"token"] forHTTPHeaderField:@"X-API-TOKEN"];
-//    
-//    [request setHTTPMethod:@"POST"];
-//    
-//    NSURLResponse *response = nil;
-//    NSError *error = nil;
-//    NSString *postString = [NSString stringWithFormat:@"login=%@&password=%@",_loginField1.text, _passwordField1.text];
-//    
-//    //            [request setValue:[NSString
-//    //                               stringWithFormat:@"%lu", (unsigned long)[postString length]]
-//    //            forHTTPHeaderField:@"Content-length"];
-//    
-//    [request setHTTPBody:[postString
-//                          dataUsingEncoding:NSUTF8StringEncoding]];
-//    json = [NSJSONSerialization JSONObjectWithData:[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error] options:NSJSONReadingMutableContainers error:&error];
-//    
-//    NSLog(@"0");
-//    
-//    if ([[json valueForKey:@"status"] isEqualToString:@"failed"]){
-//        [self errorShow:@"It seems that the entered username or password is incorrect"];
-//    }
-//    else if ([[json valueForKey:@"status"] isEqualToString:@"success"]) {
-//        NSLog(@"1");
-//        
-//        NSUserDefaults *userCache = [[NSUserDefaults standardUserDefaults] initWithSuiteName:@"group.co.getchill.chill"];
-//        [userCache setBool:true forKey:@"isAuth"];
-//        [userCache setValue:[[json valueForKey:@"response"] valueForKey:@"id_user"] forKey:@"id_user"];
-//        [userCache setValue:_loginField1.text forKey:@"login_user"];
-//        [userCache setValue:[[json valueForKey:@"response"] valueForKey:@"token"] forKey:@"token"];
-//        [userCache synchronize];
-//        
-//        PFInstallation *currentInstallation = [PFInstallation currentInstallation];
-//        [currentInstallation addUniqueObject:[NSString stringWithFormat:@"us%@",[userCache valueForKey:@"id_user"]] forKey:@"channels"];
-//        [currentInstallation saveInBackground];
-//        
-//        [self setupGAUserID: [userCache valueForKey:@"id_user"]];
-//        userCache = [[NSUserDefaults standardUserDefaults] initWithSuiteName:@"group.co.getchill.chill"];
-//        
-//        request = [[NSMutableURLRequest alloc]initWithURL:[NSURL URLWithString: [NSString stringWithFormat:@"http://api.iamchill.co/v1/users/index/id_user/%@",[userCache valueForKey:@"id_user"]]]];
-//        //[request setValue:@"Chill" forHTTPHeaderField:@"User-Agent"];
-//        [request setValue:[[NSUserDefaults standardUserDefaults] valueForKey:@"token"] forHTTPHeaderField:@"X-API-TOKEN"];
-//        response = nil;
-//        error = nil;
-//        
-//        json = [NSJSONSerialization JSONObjectWithData:[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error] options:NSJSONReadingMutableContainers error:&error];
-//        if ([[json valueForKey:@"status"] isEqualToString:@"failed"])
-//        {
-//            NSLog(@"%@", [json valueForKey:@"response"]);
-//            //[self performSegueWithIdentifier:@"AuthWaitingViewController" sender:self];
-//        }
-//        else if ([[json valueForKey:@"status"] isEqualToString:@"success"])
-//        {
-//            
-//            [userCache setValue:[[[json valueForKey:@"response"] valueForKey:@"name"]componentsJoinedByString:@""] forKey:@"name"];
-//            [userCache setValue:[[[json valueForKey:@"response"] valueForKey:@"email"]componentsJoinedByString:@""] forKey:@"email"];
-//            [userCache setValue:[[[json valueForKey:@"response"] valueForKey:@"hash"]componentsJoinedByString:@""] forKey:@"hash"];
-//            [userCache setValue:[[[json valueForKey:@"response"] valueForKey:@"key"]componentsJoinedByString:@""] forKey:@"key"];
-//            [userCache setValue:[[[json valueForKey:@"response"] valueForKey:@"date_reg"]componentsJoinedByString:@""] forKey:@"date_reg"];
-//            [userCache setValue:[[[json valueForKey:@"response"] valueForKey:@"approved"] componentsJoinedByString:@""] forKey:@"approved"];
-//            [userCache setValue:[[[json valueForKey:@"response"] valueForKey:@"token"] componentsJoinedByString:@""] forKey:@"token1"];
-//            [userCache synchronize];
-//            if ( [[userCache valueForKey:@"approved"] isEqualToString:@"1"]){
-//                
-//                [userCache setBool:true forKey:@"isApproved"];
-//                [userCache synchronize];
-//
-//    if (![_emailField.text isEqualToString:@""] && ![_passwordField.text isEqualToString:@""]){
-//        if (![self connected]) {
-//            [self conRefused];
-//        }
-//    }
-//        else {
-//            
-//        }
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc]initWithURL:[NSURL URLWithString: [NSString stringWithFormat:@"http://178.62.151.46/v1/users/update/"]]];
+//                [request setValue:@"Chill" forHTTPHeaderField:@"User-Agent"];
+    [request setValue:[[NSUserDefaults standardUserDefaults] valueForKey:@"token"] forHTTPHeaderField:@"X-API-TOKEN"];
     
+    [request setHTTPMethod:@"POST"];
+
+    NSURLResponse *response = nil;
+    NSError *error = nil;
+    NSString *postString = [NSString stringWithFormat:@"id_user=%@&email=%@&password=%@",[[NSUserDefaults standardUserDefaults] valueForKey:@"id_user"],  _emailField.text, _passwordField.text];
+    
+    //NSLog(@"ID_USER V NS USER DEFAULTS POLUCHILSYA SLEDUYUWIY: %@", [[NSUserDefaults standardUserDefaults] valueForKey:@"id_user"]);
+//    
+//                [request setValue:[NSString
+//                                   stringWithFormat:@"%lu", (unsigned long)[postString length]]
+//                forHTTPHeaderField:@"Content-length"];
+//    
+    [request setHTTPBody:[postString
+                         dataUsingEncoding:NSUTF8StringEncoding]];
+    json = [NSJSONSerialization JSONObjectWithData:[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error] options:NSJSONReadingMutableContainers error:&error];
+   
+    
+    NSLog(@"REQUEST's STATUS: %@", json);
+    
+    if ([[json valueForKey:@"status"] isEqualToString:@"failed"]){
+        [self errorShow:@"It seems that the entered email or password is incorrect"];
+    }
+    else if ([[json valueForKey:@"status"] isEqualToString:@"success"]) {
+
+        NSUserDefaults *userCache = [[NSUserDefaults standardUserDefaults] initWithSuiteName:@"group.co.getchill.chill"];
+        [userCache setValue:[[json valueForKey:@"response"] valueForKey:@"email"] forKey:@"email"];
+        [userCache setValue:[[json valueForKey:@"response"] valueForKey:@"password"] forKey:@"password"];
+
+
+        [userCache synchronize];
+        
+        PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+        [currentInstallation addUniqueObject:[NSString stringWithFormat:@"us%@",[userCache valueForKey:@"id_user"]] forKey:@"channels"];
+        [currentInstallation saveInBackground];
+        
+        [self setupGAUserID: [userCache valueForKey:@"id_user"]];
+        userCache = [[NSUserDefaults standardUserDefaults] initWithSuiteName:@"group.co.getchill.chill"];
+        
+
+
+    }
      [self dismissViewControllerAnimated:YES completion:nil];
 }
+    
+    - (void) setupGAUserID: (NSString *)userID {
+        id<GAITracker> tracker = [[GAI sharedInstance] defaultTracker];
+        
+        // You only need to set User ID on a tracker once
+        [tracker set:@"&uid" value:userID];
+        [tracker set:kGAIScreenName value:@"Auth screen"];
+        [tracker send:[[GAIDictionaryBuilder createEventWithCategory:@"UX"
+                                                              action:@"User Sign In"
+                                                               label:nil
+                                                               value:nil] build]];
+    }
 
 
 - (IBAction)logout:(id)sender {
