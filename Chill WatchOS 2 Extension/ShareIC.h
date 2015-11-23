67983ded32899d16a2144aedde8db09a5b9120b4
Михаил Луцкий <mikhail@lwts.ru>
2015-11-23 18:38:59 +0300

    watch update

--- Chill WatchOS 2 Extension/ShareIC.h
+++ Chill WatchOS 2 Extension/ShareIC.h
@@ -8,7 +8,14 @@
 
 #import <WatchKit/WatchKit.h>
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface ShareIC : WKInterfaceController
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceButton *icon1;
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceButton *icon2;
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceButton *icon3;
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceButton *icon4;
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceButton *icon5;
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceButton *icon6;
 
 @end
--- Chill WatchOS 2 Extension/ShareIC.m
+++ Chill WatchOS 2 Extension/ShareIC.m
@@ -8,7 +8,12 @@
 
 #import "ShareIC.h"
 #import "UserCache.h"
-@interface ShareIC ()
+#import "JMImageCache.h"
+#import <AFNetworking/AFNetworking.h>
+
+@interface ShareIC () {
+    NSArray *json;
+}
 
 @end
 
@@ -26,6 +31,29 @@
 - (void)willActivate {
     // This method is called when watch view controller is about to be visible to user
     [super willActivate];
+}
+
+- (void) loadData {
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    [manager.requestSerializer setValue:[NSUserDefaults userToken] forHTTPHeaderField:@"X-API-TOKEN"];
+    [manager.requestSerializer setValue:@"76eb29d3ca26fe805545812850e6d75af933214a" forHTTPHeaderField:@"X-API-KEY"];
+    [manager GET:[NSString stringWithFormat:@"http://api.iamchill.co/v2/contacts/index/id_user/%@", [NSUserDefaults userID]] parameters:nil success:^(NSURLSessionTask *task, id responseObject) {
+        if ([[responseObject objectForKey:@"status"] isEqualToString:@"success"]) {
+            json = [responseObject objectForKey:@"response"];
+            [self setIcons];
+        }
+        NSLog(@"JSON: %@", responseObject);
+    } failure:^(NSURLSessionTask *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
+}
+
+- (void) setIcons {
+    
+    
+    
 }
 
 - (void)didDeactivate {
